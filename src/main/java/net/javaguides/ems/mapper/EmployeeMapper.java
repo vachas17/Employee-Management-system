2af635354c139861344d2cd36e1b019081905e9a
Vachas Varghese <vachasvarghese@gmail.com>
2024-02-23 12:19:59 +0530

    added required files to the repository

---
+++ src/main/java/net/javaguides/ems/mapper/EmployeeMapper.java
@@ -0,0 +1,21 @@
+package net.javaguides.ems.mapper;
+import net.javaguides.ems.entity.Employee;
+import net.javaguides.ems.dto.EmployeeDto;
+public class EmployeeMapper {
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+    public static Employee mapToemployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstName(),
+                employeeDto.getLastName(),
+                employeeDto.getEmail()
+        );
+    }
+}
